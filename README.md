50ff8ad182e7f40eeb83bf6853e006fab3c370bc
deepak_commercefabric <deepak@fabric.inc>
2020-05-23 15:07:35 +0530

    Added working script

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# azure-delete-unused-disks
+#  Azure delete unused disks
 This consist python function to delete unused disks of microsoft azure 
+
+## Setup instruction
+
+- Install and setup python virtual env
+1. git clone https://github.com/regelcloud/azure-delete-unused-disks.git
+2. cd azure-delete-unused-disks
+3. python3 -m  venv venv
+4. source venv/bin/activate
+
+- Set Azure Credentials
+1. open `delete_unused_disks.py`
+2. Add below azure creds
+```
+    os.environ['AZURE_SUBSCRIPTION_ID'] = ""
+    os.environ['AZURE_CLIENT_ID'] = ""
+    os.environ['AZURE_CLIENT_SECRET'] = ""
+    os.environ['AZURE_TENANT_ID'] = ""
+```
+3. Run script to cleanup unused disks from account>
+`  python delete_unused_disks.py --resource-group <ResourceGroupName> `
+
+## Support
+In case of any issues or bugs please reach to us *sales@regelcloud.com*
---
+++ delete_unused_disks.py
@@ -0,0 +1,48 @@
+from azure.mgmt.resource import ResourceManagementClient
+# from azure.mgmt.network import NetworkManagementClient
+from azure.mgmt.compute import ComputeManagementClient
+from azure.common.credentials import ServicePrincipalCredentials
+import os
+import argparse
+
+def get_credentials():
+    subscription_id = os.environ['AZURE_SUBSCRIPTION_ID']
+    credentials = ServicePrincipalCredentials(
+        client_id=os.environ['AZURE_CLIENT_ID'],
+        secret=os.environ['AZURE_CLIENT_SECRET'],
+        tenant=os.environ['AZURE_TENANT_ID']
+    )
+    return credentials, subscription_id
+
+def run(resource_group_name):
+    credentials, subscription_id = get_credentials()
+    resource_client = ResourceManagementClient(credentials, subscription_id)
+    compute_client = ComputeManagementClient(credentials, subscription_id)
+    # network_client = NetworkManagementClient(credentials, subscription_id)
+
+    # for  item in resource_client.resource_groups.list():
+    #     print(item)
+    disks = compute_client.disks.list_by_resource_group(resource_group_name, custom_headers=None, raw=False)
+    for disk  in disks:
+        # print(disk.managed_by, disk.disk_state)
+        if not disk.managed_by and disk.disk_state == 'Unattached':
+            print("[INFO] Deleting unattached disk {} in resource group {}".format(disk.name, resource_group_name))
+            disks = compute_client.disks.delete(resource_group_name, disk.name, custom_headers=None, raw=False, polling=True)
+        else:
+            print("[INFO]  Skiiping!! Disk {} is attached  to source {} and  belongs to resource group {}".format(disk.name, disk.managed_by, resource_group_name))
+
+if __name__ ==  "__main__":
+
+    os.environ['AZURE_SUBSCRIPTION_ID'] = ""
+    os.environ['AZURE_CLIENT_ID'] = ""
+    os.environ['AZURE_CLIENT_SECRET'] = ""
+    os.environ['AZURE_TENANT_ID'] = ""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--resource-group',
+                        help='Resource Group Name')
+
+    args = parser.parse_args()
+    # print(args)
+    resource_group_name = args.resource_group
+    run(resource_group_name)
